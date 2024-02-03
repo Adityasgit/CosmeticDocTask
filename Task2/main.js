d7fec00e6c0703a23c3f75307ae1398333ddf02e
Aditya <adityaaroraaditya12@gmail.com>
2024-02-03 20:20:53 +0530

    Task 2 completed

---
+++ Task2/main.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const User = mongoose.model("User", {
+  name: String,
+  email: String,
+  Age: Number,
+});
+connectToMongoDB();
+// end point for users
+app.get("/api/users", async (req, res) => {
+  const data = await User.find();
+  res.status(200).json(data);
+});
+
+app.listen(3000, () => {
+  console.log("server listening on http://localhost:3000");
+});
+
+// connect to database
+function connectToMongoDB() {
+  console.log("connecting.....");
+  mongoose
+    .connect(
+      "mongodb+srv://adityaaroraaditya12:adiadi124124@cosmeticdoctor.xgddwfq.mongodb.net/"
+    )
+    .then(console.log("DB connected succesfully"));
+}
